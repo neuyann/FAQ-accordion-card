74f6da3fbf28746394e459d69a3a94e875b0790a
neuyann <105231863+neuyann@users.noreply.github.com>
2022-06-10 11:18:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+function myFunction() {
+  var x = document.getElementById("numberOne");
+  var y = document.getElementById("firstp");
+  var z = document.getElementById("firstPic");
+  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
+    x.style.display = "block";
+    y.style.fontWeight = 600;
+    z.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    y.style.fontWeight = 400;
+    z.style.transform = "rotate(0)";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("numberTwo");
+  var y = document.getElementById("firstp2");
+  var z = document.getElementById("secondPic");
+  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
+    x.style.display = "block";
+    y.style.fontWeight = 600;
+    z.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    y.style.fontWeight = 400;
+    z.style.transform = "rotate(0)";
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("numberThree");
+  var y = document.getElementById("firstp3");
+  var z = document.getElementById("thirdPic");
+  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
+    x.style.display = "block";
+    y.style.fontWeight = 600;
+    z.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    y.style.fontWeight = 400;
+    z.style.transform = "rotate(0)";
+  }
+}
+
+function myFunction4() {
+  var x = document.getElementById("numberFour");
+  var y = document.getElementById("firstp4");
+  var z = document.getElementById("fourthPic");
+  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
+    x.style.display = "block";
+    y.style.fontWeight = 600;
+    z.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    y.style.fontWeight = 400;
+    z.style.transform = "rotate(0)";
+  }
+}
+
+function myFunction5() {
+  var x = document.getElementById("numberFive");
+  var y = document.getElementById("firstp5");
+  var z = document.getElementById("fifthPic");
+  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
+    x.style.display = "block";
+    y.style.fontWeight = 600;
+    z.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    y.style.fontWeight = 400;
+    z.style.transform = "rotate(0)";
+  }
+}
