05a771c2f5267c7f890adf070ce731843b55c504
neuyann <105231863+neuyann@users.noreply.github.com>
2022-06-17 14:05:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,24 @@
-function myFunction() {
-  var x = document.getElementById("numberOne");
-  var y = document.getElementById("firstp");
-  var z = document.getElementById("firstPic");
-  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
-    x.style.display = "block";
-    y.style.fontWeight = 600;
-    z.style.transform = "rotate(180deg)";
-  } else {
-    x.style.display = "none";
-    y.style.fontWeight = 400;
-    z.style.transform = "rotate(0)";
-  }
-}
+$(document).ready(function (){ $('.grid-item1').click(function (){
+$('.numberOne').slideToggle(); });
 
-function myFunction2() {
-  var x = document.getElementById("numberTwo");
-  var y = document.getElementById("firstp2");
-  var z = document.getElementById("secondPic");
-  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
-    x.style.display = "block";
-    y.style.fontWeight = 600;
-    z.style.transform = "rotate(180deg)";
-  } else {
-    x.style.display = "none";
-    y.style.fontWeight = 400;
-    z.style.transform = "rotate(0)";
-  }
-}
+})
 
-function myFunction3() {
-  var x = document.getElementById("numberThree");
-  var y = document.getElementById("firstp3");
-  var z = document.getElementById("thirdPic");
-  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
-    x.style.display = "block";
-    y.style.fontWeight = 600;
-    z.style.transform = "rotate(180deg)";
-  } else {
-    x.style.display = "none";
-    y.style.fontWeight = 400;
-    z.style.transform = "rotate(0)";
-  }
-}
+$(document).ready(function (){ $('.grid-item2').click(function (){
+$('.numberTwo').slideToggle(); });
 
-function myFunction4() {
-  var x = document.getElementById("numberFour");
-  var y = document.getElementById("firstp4");
-  var z = document.getElementById("fourthPic");
-  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
-    x.style.display = "block";
-    y.style.fontWeight = 600;
-    z.style.transform = "rotate(180deg)";
-  } else {
-    x.style.display = "none";
-    y.style.fontWeight = 400;
-    z.style.transform = "rotate(0)";
-  }
-}
+})
 
-function myFunction5() {
-  var x = document.getElementById("numberFive");
-  var y = document.getElementById("firstp5");
-  var z = document.getElementById("fifthPic");
-  if (x.style.display === "none" && y.style.fontWeight !== 400 && z.style.transform !== "rotate(180)") {
-    x.style.display = "block";
-    y.style.fontWeight = 600;
-    z.style.transform = "rotate(180deg)";
-  } else {
-    x.style.display = "none";
-    y.style.fontWeight = 400;
-    z.style.transform = "rotate(0)";
-  }
-}
+$(document).ready(function (){ $('.grid-item3').click(function (){
+$('.numberThree').slideToggle(); });
+
+})
+
+$(document).ready(function (){ $('.grid-item4').click(function (){
+$('.numberFour').slideToggle(); });
+
+})
+
+$(document).ready(function (){ $('.grid-item5').click(function (){
+$('.numberFive').slideToggle(); });
+
+})
